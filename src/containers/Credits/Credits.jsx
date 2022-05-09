74092d3272fbb92a02b8bd623c02ff32054833e8
travistn <travisn714@gmail.com>
2022-05-09 14:45:11 -0700

    Credits can be sorted by movies and tv

--- src/containers/Credits/Credits.jsx
+++ src/containers/Credits/Credits.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { RiArrowDownSFill } from 'react-icons/ri';
 
 import './Credits.css';
 
@@ -12,6 +11,7 @@ const formatYear = (date) => {
 
 export const Credits = ({ personId, setMovieId, setTvId }) => {
   const [credits, setCredits] = useState();
+  const [filter, setFilter] = useState('all');
   const navigate = useNavigate();
 
   const clickHandler = (e) => {
@@ -22,6 +22,12 @@ export const Credits = ({ personId, setMovieId, setTvId }) => {
     if (mediaType === 'tv') setTvId(id);
 
     navigate(`/${mediaType}/${id}`);
+  };
+
+  const filterHandler = (e) => {
+    const filterName = e.currentTarget.value;
+
+    setFilter(filterName);
   };
 
   const getCredits = async (personId) => {
@@ -42,6 +48,14 @@ export const Credits = ({ personId, setMovieId, setTvId }) => {
     return credit.title || credit.name != null;
   });
 
+  const movieCredits = credits?.cast.filter((credit) => {
+    return credit.title != null;
+  });
+
+  const tvCredits = credits?.cast.filter((credit) => {
+    return credit.name != null;
+  });
+
   const sortedTitles = creditsContainingTitle?.sort((a, b) => {
     return a.release_date < b.first_air_date ||
       a.first_air_date < b.release_date ||
@@ -56,22 +70,38 @@ export const Credits = ({ personId, setMovieId, setTvId }) => {
       : 0;
   });
 
+  const sortedMovieTitles = movieCredits?.sort((a, b) => {
+    return a.release_date < b.release_date ? 1 : a.release_date > b.release_date ? -1 : 0;
+  });
+
+  const sortedTvTitles = tvCredits?.sort((a, b) => {
+    return a.first_air_date < b.first_air_date ? 1 : a.first_air_date > b.first_air_date ? -1 : 0;
+  });
+
+  const filterSelector =
+    filter === 'all'
+      ? sortedTitles
+      : filter === 'movies'
+      ? sortedMovieTitles
+      : filter === 'tv'
+      ? sortedTvTitles
+      : null;
+
   return (
     <>
       <div className='credits__header'>
         <h4>Acting</h4>
         <div className='credits-filter'>
-          <h5>
-            All
-            <span>
-              <RiArrowDownSFill />
-            </span>
-          </h5>
+          <select onClick={filterHandler}>
+            <option value='all'>All</option>
+            <option value='movies'>Movies</option>
+            <option value='tv'>TV</option>
+          </select>
         </div>
       </div>
       <table className='credits__table'>
         <tbody>
-          {sortedTitles?.map((credit) => (
+          {filterSelector?.map((credit) => (
             <table className='table__group'>
               <tbody key={credit?.id}>
                 <tr>
