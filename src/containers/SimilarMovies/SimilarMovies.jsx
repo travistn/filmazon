6727cf48a07933c89c9723ffc83b5e761e0164be
travistn <travisn714@gmail.com>
2022-11-04 01:06:34 -0700

    Reformatted code for easier readability

--- src/containers/SimilarMovies/SimilarMovies.jsx
+++ src/containers/SimilarMovies/SimilarMovies.jsx
@@ -1,36 +1,21 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import './SimilarMovies.css';
-
-const apiKey = process.env.REACT_APP_MOVIEDB_API_KEY;
+import { apiKey } from '../../utils/Reuseables';
 
 const SimilarMedia = ({ mediaType, showId }) => {
   const [similarMedia, setSimilarMedia] = useState([]);
   const navigate = useNavigate();
 
-  const getSimilarMedia = useCallback(
-    async (showId) => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/${mediaType}/${showId}/similar?api_key=${apiKey}&language=en-US&page=1`
-      );
-
-      const data = await response.json();
-
-      setSimilarMedia(data.results);
-    },
-    [mediaType]
-  );
-
-  const clickHandler = (e) => {
-    const id = e.currentTarget.getAttribute('value');
-
-    navigate(`/${mediaType}/${id}`);
-  };
-
   useEffect(() => {
-    getSimilarMedia(showId);
-  }, [showId, getSimilarMedia]);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/${mediaType}/${showId}/similar?api_key=${apiKey}&language=en-US&page=1`
+      )
+      .then((res) => setSimilarMedia(res.data.results));
+  }, [showId, mediaType]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -46,10 +31,9 @@ const SimilarMedia = ({ mediaType, showId }) => {
               <img
                 src={`https://www.themoviedb.org/t/p/w250_and_h141_face/${show?.backdrop_path}`}
                 alt='movie-poster'
-                onClick={clickHandler}
-                value={show?.id}
+                onClick={() => navigate(`/${mediaType}/${show.id}`)}
               />
-              <p onClick={clickHandler} value={show?.id}>
+              <p onClick={() => navigate(`/${mediaType}/${show.id}`)}>
                 {mediaType === 'movie' ? show?.title : show?.name}
               </p>
             </div>
